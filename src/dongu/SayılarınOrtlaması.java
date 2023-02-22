7e0d753ae81e662ec48073c263837fd9d0e46d64
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-22 15:50:39 +0300

    Add files via upload

---
+++ src/dongu/SayılarınOrtlaması.java
@@ -0,0 +1,28 @@
+package dongu;
+import java.util.Scanner;
+public class SayılarınOrtlaması 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Bir sayi giriniz");
+		Scanner scanner = new Scanner(System.in);
+		int sayi = scanner.nextInt();
+		int toplam =0;
+		double ortalama;
+		int sayaç =0;
+		for(int i=0; i<sayi; i++)
+		{
+			if(i%3 ==0 && i%4 ==0)
+			{
+				toplam = toplam +i;
+				sayaç++;
+			}
+		
+		}
+		ortalama = toplam / sayaç;
+		System.out.println(ortalama);
+
+	}
+
+}
